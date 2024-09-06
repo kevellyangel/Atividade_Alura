5aea190fe5f4799d4effb78b233aff85de357149
kevellyangel <74574967+kevellyangel@users.noreply.github.com>
2024-09-06 18:23:00 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao:"Rayssa Leal,a Fadinha,é uma skatista brasileira que",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+        img: "https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2024/07/28/796112609-rayssa-leal-medalha-paris-scaled-aspect-ratio-512-320.jpg",
+    },
+
+    {
+        titulo: "Beatriz Souza",
+        descricao:"Beatriz Souza é uma judoca brasileira que se destacou",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka",
+        img: "https://midias.correiobraziliense.com.br/_midias/jpg/2024/08/02/1000x1000/1_whatsapp_image_2024_08_02_at_13_32_49-39247688.jpeg?20240802133614?20240802133614",
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao:"Rebeca Andrade é uma ginasta brasileira que conquistou ouro",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro",
+        img: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Rebeca_Andrade_in_2023.jpg",
+    },
+
+];
